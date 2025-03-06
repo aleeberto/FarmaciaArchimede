fee81ea7870ab308cf87307a81525a2274d72680
aleeberto <alebertolazzi19@gmail.com>
2025-03-06 16:37:16 +0000

    Update BreadCrumble

--- php/pages/areapersonale.php
+++ php/pages/areapersonale.php
@@ -5,7 +5,7 @@ $page_index = 4;
 
 require_once($root . "/php/main.php");
 
-$prodotti_template = $builder->load_template("areapersona.html");
+$prodotti_template = $builder->load_template("areapersonale.html");
 $prodotti_template->insert("header", build_header());
 $prodotti_template->insert("footer", build_footer());
 
--- php/pages/chisiamo.php
+++ php/pages/chisiamo.php
@@ -5,7 +5,7 @@ $page_index = 2;
 
 require_once($root . "/php/main.php");
 
-$prodotti_template = $builder->load_template("cisiamo.html");
+$prodotti_template = $builder->load_template("chisiamo.html");
 $prodotti_template->insert("header", build_header());
 $prodotti_template->insert("footer", build_footer());
 
