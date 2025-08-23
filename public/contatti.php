a4339b131898908e43428b475066148bbd855fff
aleeberto <alebertolazzi19@gmail.com>
2025-08-23 15:08:53 +0200

    Rebuild css, keywords and title

--- public/contatti.php
+++ public/contatti.php
@@ -5,6 +5,6 @@ use App\Core\PageBuilder;
 
 PageBuilder::show($_SERVER['SCRIPT_NAME'], [
     'meta_title'       => 'Contatti | Farmacia Archimede',
-    'meta_description' => 'Descrizione specifica per questa pagina',
-    'meta_keywords'    => 'parola1, parola2, parola3'],
+    'meta_description' => 'Pagina dei contatti a cui è possibile rivolgersi per informazioni sulla Farmacia Archimede',
+    'meta_keywords'    => 'contatti, farmacia, archimede, assistenza, informazioni'],
 safe: true);
