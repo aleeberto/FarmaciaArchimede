bd417e7e2d0a24c1a7ca465d3e1869101545f7cc
Guglielmo Barison <guglielmobarison00@gmail.com>
2025-08-24 23:11:29 +0200

    merge guglielmo e alessandro

--- public/index.php
+++ public/index.php
@@ -7,4 +7,4 @@ PageBuilder::show($_SERVER['SCRIPT_NAME'], [
     'meta_title'       => 'Home | Farmacia Archimede',
     'meta_description' => 'Pagina di benvenuto della Farmacia Archimede',
     'meta_keywords'    => 'home, farmacia, archimede, benvenuto, salute',
-];
+]);
